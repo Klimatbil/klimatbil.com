dd4bc25ce9e4516b8dd49e3c9da2f76efe57896c
Marcus Otterström <marcus.otterstrom@gmail.com>
2018-10-17 20:25:53 +0200

    Add simple demo
    Made so testing can be made in github pages

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function()
+{
+    /* jQuery test */
+    let header = $("#js-header");
+    header.click(
+        function()
+        {
+            location.href = "../aboutus.html"
+        });
+
+    header.hover(
+        function()
+        {
+            $(this).addClass("highlighted")
+        }, 
+        function()
+        {
+            $(this).removeClass("highlighted")
+        });
+})
