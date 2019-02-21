4c059ad0b57b0d2777a00b5c25040d248cd7b3e7
Marcus Otterström <marcus.otterstrom@gmail.com>
2019-02-21 16:24:29 +0100

    Major clean up and style changes

--- js/main.js
+++ js/main.js
@@ -23,15 +23,3 @@ function ToggleNavBar()
     let overlapHide = document.getElementById("js-overlap-hide");
     overlapHide.classList.toggle("hidden");
 }
-
-/**
- * Toggles the visibility of the modal drawer and elements that relate to it (the reset div, for example). 
- */
-/*
-function ToggleDrawer()
-{
-    document.getElementById("js-drawer").classList.toggle("drawer-active");
-    document.getElementById("js-drawer-hide-background").classList.toggle("transparent-background");
-    document.getElementById("js-drawer-hide").classList.toggle("hidden");
-}
-*/
